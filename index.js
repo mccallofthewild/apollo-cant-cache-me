a538488ea41ad45440db49379b19c5495d9c5c13
mccallofthewild <mccallalexander96@gmail.com>
2018-01-07 11:57:06 -0700

    adding all

---
+++ index.js
@@ -0,0 +1,8 @@
+export default client => {
+    const apolloQuery = client.query.bind(client);
+    client.query = async (...args) => {
+        await client.cache.reset();
+        return apolloQuery(...args);
+    };
+    return client;
+};
